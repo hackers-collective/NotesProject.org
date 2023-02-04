05fdbf745009d17885580cfe682f6f051a948ab5
Shobhit Sharma <shobhit0812@gmail.com>
2023-02-04 18:40:24 +0530

    Update App.js

--- build/src/App.js
+++ build/src/App.js
@@ -1,17 +1,15 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       NotesProject.org Deployment
-  //     </header>
-  //   </div>
-  // );
-
-  return null;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        NotesProject.org Deployment
+      </header>
+    </div>
+  );
 }
 
 export default App;
