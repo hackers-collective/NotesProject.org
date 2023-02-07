c5acf9b6055af54a5069a6fadeb78a7dd923fee9
Shobhit Sharma <shobhit0812@gmail.com>
2023-02-07 22:00:33 +0530

    clean

--- build/src/App.js
+++ build/src/App.js
@@ -1,17 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          NotesProject.org
-        </p>
-        
-      </header>
-    </div>
+   <h1>
+      NotesProject.org
+   </h1>
   );
 }
 
