c6f74b4831be325a57e042225a7032539103c69a
Shobhit Sharma <shobhit0812@gmail.com>
2023-04-02 16:56:19 +0530

    Update App.js

--- build/src/App.js
+++ build/src/App.js
@@ -7,12 +7,28 @@ const font = {
   fontFamily: 'Lexend Deca'
 }
 
+const banner = {
+  fontSize: '120px'
+}
+
+const out = {
+  color: "#2222FF",
+  fontSize: '120px'
+}
+
+const NP = {
+  padding: "25px 50px 75px 100px"
+}
+
 function App() {
   return (
     <div style={font}>
-      <h1>
-      NotesProject.org
+      <h1 style={banner}>
+      CUT THE SHIT <span style={out}>OUT</span>
       </h1>
+      <h2 style={NP}>
+      NotesProject.org
+      </h2>
     </div>
    
   );
