ef1d26d88e9e051c5fd89e2d79650c990074ef63
Shobhit Sharma <shobhit0812@gmail.com>
2023-05-26 09:45:20 +0530

    Update index.js

--- build/src/index.js
+++ build/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const NotFound = () => (
+  <div>
+    <h1>404 - Page Not Found</h1>
+    <p>The requested page does not exist.</p>
+  </div>
+);
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
