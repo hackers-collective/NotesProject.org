8070339a313fb4e187c3560ef5a7c6c95bf7c312
Shobhit Sharma <shobhit0812@gmail.com>
2023-05-27 12:02:36 +0530

    Update Visualizer.js

--- build/src/Visualizer.js
+++ build/src/Visualizer.js
@@ -20,10 +20,10 @@ const headingStyle = {
 
 const gridContainerStyle = {
   display: 'grid',
-  gridTemplateColumns: 'repeat(auto-fit, minmax(80px, 1fr))', // Responsive grid columns
+  gridTemplateColumns: 'repeat(10, 1fr)',
   gridGap: '10px',
-  maxWidth: '800px', // Limit the maximum width of the grid
-  width: '100%', // Ensure the grid takes up the full width of the container
+  maxWidth: '800px',
+  width: '100%',
 };
 
 const gridItemStyle = {
@@ -32,17 +32,9 @@ const gridItemStyle = {
   justifyContent: 'center',
   backgroundColor: '#f1f1f1',
   color: '#333333',
-  paddingTop: '100%', // Maintain the square shape of the grid items
-  position: 'relative',
+  height: '80px',
   fontFamily: 'Lexend Deca',
-  fontSize: '16px', // Adjust font size for better readability on mobile devices
-};
-
-const gridItemContentStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  fontSize: '16px',
 };
 
 function Visualizer() {
@@ -51,7 +43,7 @@ function Visualizer() {
     for (let i = 0; i < 100; i++) {
       grids.push(
         <div key={i} style={gridItemStyle}>
-          <div style={gridItemContentStyle}>{i + 1000}</div>
+          {i + 1000}
         </div>
       );
     }
