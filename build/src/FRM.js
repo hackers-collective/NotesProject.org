5222593171a74d19271e2cf6f820a0f2e236b2b3
Shobhit Sharma <shobhit0812@gmail.com>
2023-11-26 22:26:13 +0530

    Create FRM.js

---
+++ build/src/FRM.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+// TXNs API/Query
+
+const txns = [
+  { mode: 'UPI', isOTP: false, id: 138292 },
+  { mode: 'WALLET', isOTP: true, id: 229383 },
+  { mode: 'ECOM', isOTP: true, id: 200393 },
+];
+
+export default function Transactions() {
+  const txnList = txns.map(txn =>
+    
+    <>
+    <h1 key={txn.id}
+      style={
+        {
+          color: txn.isOTP ? 'red' : 'darkgreen'
+        }, 
+        {
+          fontStyle: txn.isOTP? 'italic' : ''
+        }
+      }
+    >
+      {txn.mode}
+    </h1>
+{/*     <br /> */}
+      </>
+      
+  );
+
+  return (
+    <>
+    {txnList}
+    </>
+  );
+}
